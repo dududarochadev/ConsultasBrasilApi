fc672624ea1e82099a911d9fa3ea3206da9b00f5
Dudu da Rocha <eduardoldarocha@gmail.com>
2023-08-19 00:25:00 -0300

    refactor: ajustando importações de classe

--- consultas-brasil-api/src/main/java/br/dududarochadev/consultasbrasilapi/controllers/HomeController.java
+++ consultas-brasil-api/src/main/java/br/dududarochadev/consultasbrasilapi/controllers/HomeController.java
@@ -1,8 +1,13 @@
-import org.springBootFramework.stereotype.Controller;
+package br.dududarochadev.consultasbrasilapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @GetMapping
     public String Home() {
         return "home";
     }
